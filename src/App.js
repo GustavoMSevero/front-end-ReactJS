e23a0baf05219671d184f7bb7e24f3bbe66c4be1
Gustavo M. Severo <gustavo@MacBook-Air-de-Flavia.local>
2020-08-20 16:14:20 -0300

    Aula ReactJS finalizada e funcionando

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import api from './services/api';
 
 import './App.css';
@@ -6,12 +6,24 @@ import './App.css';
 import Header from './component/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
 
-    function handleAddProject() {
-        // projects.push(`Novo projeto ${Date.now()}`);
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
 
-        setProjects([...projects, `Novo projeto ${Date.now()}`])
+    async function handleAddProject() {
+        // setProjects([...projects, `Novo projeto ${Date.now()}`])
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Gustavo Severo"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
 
     return (
@@ -19,7 +31,7 @@ function App() {
             <Header title="Projects" />
 
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
