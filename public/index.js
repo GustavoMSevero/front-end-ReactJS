43d92771f925e350385b5a332935cf1dc6d3d4da
Gustavo M. Severo <gustavo@MacBook-Air-de-Flavia.local>
2020-08-20 12:33:19 -0300

    front-end ReactJS repositório

---
+++ public/index.js
@@ -0,0 +1,6 @@
+/**
+ * Babel Converter (transpilar) código do React para um código que o browser entenda
+ * webpack: Para cara tipo de arquivo (.js, .css, .png) eu vou converter o código de uma maneira diferente
+ * 
+ * Loaders: babel-loader, css-loader, image-loadaer
+ */
